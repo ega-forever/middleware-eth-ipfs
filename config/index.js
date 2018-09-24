b8d34d7398b5417e63aa279aa89e88cf71f21f44
kirya <cloudkserg11@gmail.com>
2018-09-24 17:10:02 +0300

    https://chronobank.atlassian.net/browse/MD-688 - parseInt to CHECK_SYSTEM

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ const config = {
     exchange: process.env.SYSTEM_RABBIT_EXCHANGE || 'internal',
     serviceName: process.env.SYSTEM_RABBIT_SERVICE_NAME || 'system' 
   },
-  checkSystem: process.env.CHECK_SYSTEM || true,
+  checkSystem: process.env.CHECK_SYSTEM ? parseInt(process.env.CHECK_SYSTEM) : true,
   schedule: {
     pinJob: process.env.SCHEDULE_JOB ||  process.env.SCHEDULE_PIN_JOB || '30 * * * * *',
     fetchJob: process.env.SCHEDULE_JOB || process.env.SCHEDULE_FETCH_JOB || '30 * * * * *'
